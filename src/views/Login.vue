bd70a8715ff5a10e7a8080aa3079846b15a13dc3
faymi <faymilin@outlook.com>
2017-12-10 16:21:37 +0800

    M 对接登录接口

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,15 +2,23 @@
   <div class="login-content">
     <div class="login-conwrap">
       <p>idea2t</p>
-      <div class="ipt-wrap">
-        <el-input placeholder="用户名" v-model="username"><el-button slot="prepend" icon="fa fa-user-o fa-fw"></el-button></el-input>
-      </div>
-      <div class="ipt-wrap">
-        <el-input placeholder="密码" v-model="password" type="password"><el-button slot="prepend" icon="fa fa-key fa-fw"></el-button></el-input>        
-      </div>
-      <div class="ipt-wrap">
-        <el-button class="login-cls" @click="login">登&nbsp;&nbsp;&nbsp;&nbsp;录</el-button>
-      </div>
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="0px">
+        <div class="ipt-wrap">
+          <el-form-item prop="username">
+            <el-input placeholder="用户名" v-model="ruleForm.username" tabindex="1" clearable><el-button slot="prepend" icon="fa fa-user-o fa-fw"></el-button></el-input>
+          </el-form-item>
+        </div>
+        <div class="ipt-wrap">
+          <el-form-item prop="password">
+            <el-input placeholder="密码" v-model="ruleForm.password" type="password" @keyup.enter.native="login('ruleForm')" tabindex="2" clearable><el-button slot="prepend" icon="fa fa-key fa-fw"></el-button></el-input>        
+          </el-form-item>
+        </div>
+        <div class="ipt-wrap">
+          <el-form-item>
+            <el-button class="login-cls" @keyup.enter.native="login('ruleForm')" @click="login('ruleForm')">登&nbsp;&nbsp;&nbsp;&nbsp;录</el-button>
+          </el-form-item>
+        </div>
+      </el-form>
     </div>
   </div>
 </template>
@@ -19,14 +27,66 @@
 export default {
   name: 'Login',
   data () {
+    // 校验字段
+    let validateUsername = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入用户名'))
+      } else {
+        callback()
+      }
+    }
+    let validatePassword = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        callback()
+      }
+    }
     return {
-      username: '',
-      password: ''
+      ruleForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+            { validator: validateUsername, trigger: 'blur' }
+        ],
+        password: [
+          { validator: validatePassword, trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
-    login () {
-      this.$router.push('/home')
+    // 登录
+    login (formName) {
+      this.$refs[formName].validate((valid) => {
+        let _this = this
+        if (valid) {
+          let username = this.ruleForm.username
+          let password = this.ruleForm.password
+          this.axios.post('/ideat/userManage/login', {
+            userId: username,
+            pwd: password
+          })
+          .then(function (response) {
+            let data = response.data
+            if (data.code !== 0) {
+              _this.$notify.error({
+                title: '温馨提示',
+                message: data.msg
+              })
+              return
+            }
+            _this.$router.push('/home')
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+        } else {
+          return false
+        }
+      })
     }
   }
 }
