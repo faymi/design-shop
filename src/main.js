e6d40e047f5b3eb714035b05c5fa47a49fec5698
faymi <faymilin@outlook.com>
2017-12-11 23:29:30 +0800

    + 添加请求和响应拦截器

--- src/main.js
+++ src/main.js
@@ -9,6 +9,51 @@ import 'element-ui/lib/theme-chalk/index.css'
 import 'font-awesome/css/font-awesome.css'
 import './assets/css/reset.scss'
 
+// 添加请求拦截器
+axios.interceptors.request.use(
+  config => {
+    // let xtoken = getXtoken()
+    // if (xtoken != null) {
+    //   config.headers['X-Token'] = xtoken
+    // }
+    // if (config.method === 'post') {
+    //   config.data = {
+    //     ...config.data,
+    //     _t: Date.parse(new Date()) / 1000
+    //   }
+    // } else if (config.method === 'get') {
+    //   config.params = {
+    //     _t: Date.parse(new Date()) / 1000,
+    //     ...config.params
+    //   }
+    // }
+    if (config.method === 'get') {
+      config.params = {
+        _t: Date.parse(new Date()) / 1000,
+        params: {...config.params}
+      }
+    }
+    return config
+  }, function (error) {
+  return Promise.reject(error)
+})
+
+// 添加响应拦截器
+// axios.interceptors.response.use(function (response) {
+// // token 已过期，重定向到登录页面
+//   if (response.data.code === 4) {
+//     localStorage.clear()
+//     router.replace({
+//       path: '/signin',
+//       query: {redirect: router.currentRoute.fullPath}
+//     })
+//   }
+//   return response
+// }, function (error) {
+// // Do something with response error
+//   return Promise.reject(error)
+// })
+
 Vue.prototype.axios = axios
 
 Vue.config.productionTip = false
