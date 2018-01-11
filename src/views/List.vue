d03100d83ad0ae66cf8dcae19db9f550df0b5182
faymi <faymilin@outlook.com>
2018-01-11 11:20:02 +0800

    M 调整商品列表页样式

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="list-wrap">
-    <v-header :logo-img="logo" :shop-name="shopName"></v-header>
     <div class="content-wrap clearfix">
-      <ul>
-        <li v-for="item in goodsList" :key="item.goodsId">
-          <img class="cloth-img" :src="item.goodsPicPath" @click="toDetail">
-          <div class="item-cls">
-            <div class="item-name">
-              <span>{{item.goodsName}}</span>
-              <span>￥{{item.goodsPrice}}</span>
+      <v-header :logo-img="logo" :shop-name="shopName"></v-header>
+      <div class="ul-wrap">
+        <ul>
+          <li v-for="item in goodsList" :key="item.goodsId">
+            <img class="cloth-img" :src="item.goodsPicPath" @click="toDetail">
+            <div class="item-cls">
+              <div class="item-name">
+                <span>{{item.goodsName}}</span>
+                <span>￥{{item.goodsPrice}}</span>
+              </div>
+              <div class="item-btm">
+                <router-link to="/customized">
+                  <button @click="toDesign(item.goodsId)">定制此款</button>
+                </router-link>
+              </div>
             </div>
-            <div class="item-btm">
-              <router-link to="/customized">
-                <button @click="toDesign(item.goodsId)">定制此款</button>
-              </router-link>
-            </div>
-          </div>
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </div>
       <div v-show="goodsList.length == 0"><i class="fa fa-info"></i>&nbsp;暂无该类商品</div>
+      <v-footer></v-footer>
     </div>
-    <v-footer></v-footer>
   </div>
 </template>
 
@@ -80,48 +82,52 @@ export default {
     min-height: 100%;
     width: 96%;
     text-align: center;
-    margin-top: px2rem(116px);
-    ul {
-      margin: 0 auto;
-      padding-bottom: px2rem(120px);
-      display: flex;
-      justify-content: space-between;
-      flex-wrap: wrap;
-      li {
-        width: px2rem(320px);
-        height: px2rem(430px);
-        margin: px2rem(16px);
-        border: px2rem(2px) solid #ececec;
-        .cloth-img {
-          width: 100%;
-          height: px2rem(320px);
-        }
-        .item-cls {
-          text-align: center;
-          margin-top: px2rem(8px);
-          .item-name {
-            display: flex;
-            justify-content: space-between;
-            height: px2rem(40px);
+    position: relative;
+    .ul-wrap {
+      position: absolute;
+      top: px2rem(112px);
+      bottom: px2rem(112px);
+      overflow: auto;
+      ul {
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        li {
+          width: px2rem(320px);
+          height: px2rem(430px);
+          margin: px2rem(16px);
+          border: px2rem(2px) solid #ececec;
+          .cloth-img {
             width: 100%;
-            span {
-              padding-left: px2rem(10px);
-              padding-right: px2rem(10px);            
+            height: px2rem(320px);
+          }
+          .item-cls {
+            text-align: center;
+            margin-top: px2rem(8px);
+            .item-name {
+              display: flex;
+              justify-content: space-between;
+              height: px2rem(40px);
+              width: 100%;
+              span {
+                padding-left: px2rem(10px);
+                padding-right: px2rem(10px);            
+              }
+            }
+            button {
+              border: none;
+              background-color: $btn-color;
+              border-radius: px2rem(30px);
+              width: px2rem(160px);
+              height: px2rem(46px);
+              color: $btn-font-color;
+              font-size: $btn-font-size;
             }
           }
-          button {
-            border: none;
-            background-color: $btn-color;
-            border-radius: px2rem(30px);
-            width: px2rem(160px);
-            height: px2rem(46px);
-            color: $btn-font-color;
-            font-size: $btn-font-size;
-          }
         }
-      }
+      } 
     }
-    
   }
 }
 </style>
