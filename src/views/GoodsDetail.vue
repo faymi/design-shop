b34f8fee517d15b39b812e064852be0fa8df715e
faymi <faymilin@outlook.com>
2018-01-24 17:56:09 +0800

    M 调整订单详情页面样式

--- src/views/GoodsDetail.vue
+++ src/views/GoodsDetail.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="detail-wrap">
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        <van-swipe>
-          <van-swipe-item v-for="(str, index) in listImg" :key="index">
-            <img v-lazy="str.goodsPicPath" />
-          </van-swipe-item>
-        </van-swipe>
+    <div class="wrap-view">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <van-swipe>
+            <van-swipe-item v-for="(str, index) in listImg" :key="index">
+              <img v-lazy="str.goodsPicPath" />
+            </van-swipe-item>
+          </van-swipe>
+        </div>
       </div>
-    </div>
-    <div class="goods-name">
-      <span>{{goodsName}}</span>
-      <span>￥{{price}}</span>
-    </div>
-    <div class="print-skill">
-      <span><b>印刷工艺：</b>{{printSkill}}</span>
-    </div>
-    <div class="goods-detail">
-      <span><b>商品简介：</b>{{goodsDescript}}</span>
-    </div>
-    <div class="bottom-btn">
-      <button @click="toDesign">立即定制</button>
+      <div class="goods-name">
+        <span>{{goodsName}}</span>
+        <span>￥{{price}}</span>
+      </div>
+      <div class="print-skill">
+        <span><b>印刷工艺：</b>{{printSkill}}</span>
+      </div>
+      <div class="goods-detail">
+        <span><b>商品简介：</b>{{goodsDescript}}</span>
+      </div>
+      <div class="bottom-btn">
+        <button @click="toDesign">立即定制</button>
+      </div>
     </div>
   </div>
 </template>
@@ -89,50 +91,54 @@ export default {
 <style lang="scss" scoped>
 @import '../assets/css/function';
 .detail-wrap {
-  .swiper-container {
-    width: 100%;
-    height: px2rem(750px);
-    .swiper-wrapper {
+  .wrap-view {
+    padding-bottom: px2rem(114px);
+    overflow: auto;
+    .swiper-container {
       width: 100%;
-      height: 100%;
-      .van-swipe {
+      height: px2rem(750px);
+      .swiper-wrapper {
         width: 100%;
         height: 100%;
-        background-size: cover;
-        .van-swipe__track .van-swipe-item img {
-          height: 100%;
+        .van-swipe {
           width: 100%;
+          height: 100%;
           background-size: cover;
+          .van-swipe__track .van-swipe-item img {
+            height: 100%;
+            width: 100%;
+            background-size: cover;
+          }
         }
       }
     }
-  }
-  .goods-name {
-    display: flex;
-    justify-content: space-between;
-    margin: px2rem(20px);
-    span {
-      &:first-child {
-        font-size: 16px;
+    .goods-name {
+      display: flex;
+      justify-content: space-between;
+      margin: px2rem(20px);
+      span {
+        &:first-child {
+          font-size: 16px;
+          font-weight: bold;
+        }
+      }
+      span {
+        &:nth-child(2) {
+          font-size: 16px;
+        }
+      }
+    }
+    .print-skill, .goods-detail {
+      text-align: left;
+      margin: px2rem(20px);
+      b {
         font-weight: bold;
       }
     }
-    span {
-      &:nth-child(2) {
-        font-size: 16px;
+    .goods-detail {
+      span {
+        line-height: px2rem(46px);
       }
-    }
-  }
-  .print-skill, .goods-detail {
-    text-align: left;
-    margin: px2rem(20px);
-    b {
-      font-weight: bold;
-    }
-  }
-  .goods-detail {
-    span {
-      line-height: px2rem(46px);
     }
   }
 }
